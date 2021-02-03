71818b2c603b731e2250d5534b400cb6aceff28e
philliphnguyen <phillip.nguyen01@sjsu.edu>
2021-02-03 13:23:56 -0800

    passport-jwt for login/signup

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const cors = require('cors');
+const passport = require('passport');
 const PORT = process.env.PORT || 3000;
 
+//Able to read variables from .env file
 require('dotenv').config();
+
+//Fixes cors errors
 app.use(cors());
 
+//Connect to database
 const db_string = process.env.DB_STRING;
-mongoose.connect(db_string, { useNewUrlParser: true, useUnifiedTopology: true}).then(
+mongoose.connect(db_string, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(
     () => { console.log("Database connection successful") },
     err => { console.log("Database connection failed " + err)}
 )
 
+//Parse json and form input
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+
+require('./models/User');
+
+require('./config/passport');
+app.use(passport.initialize());
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     email: {
         type: String,
         required: true,
-        unique: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+        unique: true
     },
     password: { type: String, required: true}
 });
