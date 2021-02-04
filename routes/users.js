bee007a7c7f5a465a134e19970230db6a97c97cd
philliphnguyen <phillip.nguyen01@sjsu.edu>
2021-02-03 18:18:04 -0800

    server-side validation

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 const jsonwebtoken = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
 
 const issueJWT = user => {
     const payload = {
@@ -26,32 +28,51 @@ const issueJWT = user => {
 };
 
 //Create a user
-router.post('/register', (req, res) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err)
-            res.status(500).json({ error: err });
+router.post(
+    '/register',
+    body('email').isEmail(),
+    body('password').isLength({ min: 5}), 
+    (req, res) => {
+    User.findOne({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if (user)
+            return res.status(409).json({ success: false, msg: "Email exists"});
         else
         {
-            const newUser = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
+            const errors = validationResult(req);
+            if (!errors.isEmpty())
+                return res.status(400).json({ errors: errors.array() });
+            
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err)
+                    res.status(500).json({ error: err });
+                else
+                {
+                    const newUser = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    });
+        
+                    newUser.save()
+                    .then(user => {
+                        const jwt = issueJWT(user);
+        
+                        res.json({ 
+                            success: true,
+                            user: user,
+                            token: jwt.token,
+                            expirsIn: jwt.expires
+                        })
+                    })
+                    .catch(err => res.json({ error: err }));
+                }
             });
-
-            newUser.save()
-            .then(user => {
-                const jwt = issueJWT(user);
-
-                res.json({ 
-                    success: true,
-                    user: user,
-                    token: jwt.token,
-                    expirsIn: jwt.expires
-                })
-            })
-            .catch(err => res.json({ error: err }));
         }
-    });
+    })
+    .catch(err => res.json({ error: err}));
+
 });
 
 //Login a user
@@ -83,6 +104,11 @@ router.post('/login', (req, res) => {
         });
     })
     .catch(err => res.json({ error: err }));
+});
+
+//Test authenticated route
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.status(200).json({ success: true, msg: "You are authorized"});
 });
 
 
