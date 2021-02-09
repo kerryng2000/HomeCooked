6141abe6ef56ab7c1d1a1543f8b0b5b21461acd9
philliphnguyen <phillip.nguyen01@sjsu.edu>
2021-02-08 22:10:56 -0800

    jwt stored in httponly cookie for security

--- client/src/actions/userActions.tsx
+++ client/src/actions/userActions.tsx
@@ -2,7 +2,21 @@ import axios from 'axios';
 import userInterface from '../interfaces/userInterface'
 
 export const register = (user: userInterface) => {
-    axios.post("/users/register", user)
-    .then(res => console.log(res))
+    axios.post("/users/register", user, { withCredentials: true })
+    .then(res => {
+        console.log(res.data);
+    })
     .catch(err => console.log(err));
+}
+
+export const checkAuth = () => {
+    axios.get("/users/checkAuth")
+    .catch(() => console.log("User is authenticated"))
+    .then(err => console.log(err));
+}
+
+export const signOut = () => {
+    axios.get("/users/signOut")
+    .catch(() => console.log("Signed out"))
+    .then(err => console.log(err));
 }
