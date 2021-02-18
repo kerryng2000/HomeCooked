097aa6a194bea671b47f214e856fd14a01ff8b46
philliphnguyen <phillip.nguyen01@sjsu.edu>
2021-02-18 14:13:12 -0800

    updated tab navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,57 +1,77 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-import Register from './pages/Register';
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabButton, IonTabs, IonTabBar } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import { home, person } from 'ionicons/icons';
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { checkAuth } from './actions/userActions';
-import SignIn from './pages/SignIn';
-import TabBar from './components/TabBar';
+import "./theme/variables.css";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { checkAuth } from "./actions/userActions";
+import SignIn from "./pages/SignIn";
+import { AppState } from "./reducers";
+import Account from "./pages/Account";
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    console.log("hello");
     dispatch(checkAuth());
-  })
-  
+  }, []);
+
+  const {isAuthenticated} = useSelector((state: AppState) => state.user);
+
   return (
     <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/register" component={Register} exact></Route>
-        <Route path="/signIn" component={SignIn} exact></Route>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-      <TabBar />
-    </IonReactRouter>
-  </IonApp>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route path="/register" component={Register} exact></Route>
+            <Route path="/signIn" component={SignIn} exact></Route>
+            <Route path="/account" exact>
+              {isAuthenticated ? <Account/> : <Redirect to="signIn"/>}
+            </Route>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
+          </IonRouterOutlet>
+         
+          <IonTabBar slot="bottom">
+                <IonTabButton tab="home" href="/home">
+                    <IonIcon icon={ home } />
+                    <IonLabel>Home</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="account" href={isAuthenticated ? "/account" : "/signin"}>
+                    <IonIcon icon={ person } />
+                    <IonLabel>Account</IonLabel>
+                </IonTabButton>
+            </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+    </IonApp>
   );
-  };
+};
 
 export default App;
--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,24 +1,15 @@
 import { IonButton, IonInput, IonItem, IonLabel, IonPage } from "@ionic/react";
-import { register, signIn } from '../actions/userActions';
-import { useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { signIn } from '../actions/userActions';
+import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { AppState } from '../reducers';
+import { IonRouterLink } from "@ionic/react";
 
 const SignIn: React.FC = () => {
     const emailInputRef = useRef<HTMLIonInputElement>(null);
     const passwordInputRef = useRef<HTMLIonInputElement>(null);
     const dispatch = useDispatch();
     const history = useHistory();
-
-    const {isAuthenticated} = useSelector((state: AppState) => state.user);
-
-    /*useEffect(() => {
-        console.log("sign in effect")
-        if (isAuthenticated)
-            history.push("/home");
-    }, [isAuthenticated])*/
-
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -46,6 +37,7 @@ const SignIn: React.FC = () => {
           <IonInput type="password" ref={ passwordInputRef }></IonInput>
         </IonItem>
         <IonButton className="ion-margin-top" type="submit">Sign in</IonButton>
+        <IonRouterLink href="/register">Don't have an account? Register here</IonRouterLink>
       </form>
     </IonPage>
   );
