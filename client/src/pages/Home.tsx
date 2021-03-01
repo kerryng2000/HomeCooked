7821b1da2378410ebcad784b6fb39d0065eae1da
kerryng2000 <33183687+kerryng2000@users.noreply.github.com>
2021-03-01 12:04:54 -0800

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,27 +1,11 @@
-import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonFooter } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonPage} from '@ionic/react';
 
 const Home: React.FC = () => {
-  const [searchText, setSearchText] = useState('');
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>HomeCooked</IonTitle>
-        </IonToolbar>
-        <IonToolbar>
-            <IonSearchbar value={ searchText } onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
-          </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">HomeCooked</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <h1>Home page</h1>
       </IonContent>
     </IonPage>
   );
