fb3ae1cbd623b0a376cd3e714f5eed9732730762
philliphnguyen <phillip.nguyen01@sjsu.edu>
2021-03-08 18:00:03 -0800

    frontend: adding dish requires account

--- client/src/pages/Dish.tsx
+++ client/src/pages/Dish.tsx
@@ -1,25 +1,27 @@
 import { IonList, IonPage, IonItem, IonContent, IonRouterLink, IonImg, IonRouterOutlet} from "@ionic/react"
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import { useSelector } from "react-redux";
+import { AppState } from "../reducers";
 
 const Dish: React.FC = () => {
+  const isAuthenticated = useSelector((state: AppState) => state.user.isAuthenticated);
 
-
-  const [dishes, setDishes] = useState([]);
+  const [dishes, setDishes] = useState<any[]>([]);
   useEffect(() => {
     sendGetRequest().then(data => setDishes(data));
   }, []);
       return (
         <IonPage>
-          <IonContent color = "primary" >
-            <IonList color = "primary">
+          <IonContent >
+            <IonList >
             {
                 dishes.map(dish => {
-                    return (
+                  return (
                       <IonItem>
                         Name: {dish['name']}
                         Price: {dish['price']}
-                        Chef: {dish['chef']}
+                        Chef: {dish['chef'].firstName} {dish['chef'].lastName}
                         <IonImg src={`../../../${dish['foodPicture']}`}>
                         </IonImg>
                         <IonRouterLink href= {`/Dishes/${dish['_id']}`}>Order</IonRouterLink>
@@ -29,15 +31,16 @@ const Dish: React.FC = () => {
             }
             </IonList>
           </IonContent>
-          <IonRouterLink href="/dish/AddDish">Add Dish here</IonRouterLink>
+          {isAuthenticated && <IonRouterLink href="/dish/AddDish">Add Dish here</IonRouterLink>}
+
       </IonPage>
 ) }
+
 const sendGetRequest = () => {
   return axios({
     url: "/Dishes/allDishes",
     method: 'get'
   }).then(response => {
-    console.log(response);
     return response.data;
   })
 };
