14969567d007a9633a9ece999e5bd95869f98742
kerryng2000 <kerry.ng@sjsu.edu>
2021-04-25 13:59:26 -0700

    Messaging & Mailbox

--- client/src/pages/Message.tsx
+++ client/src/pages/Message.tsx
@@ -12,30 +12,29 @@ import {
   } from "@ionic/react";
   import { useRef } from "react";
   import React, { useState, useEffect} from "react";
-import { useDispatch } from "react-redux";
+  import { useParams } from "react-router-dom";
+  import { useDispatch, useSelector } from "react-redux";
 import { Add } from "../actions/messageActions";
 import { useHistory } from "react-router-dom";
+import { AppState } from "../reducers";
   
   const Message: React.FC = () => {
-
-    const dispatch = useDispatch();
-    const history = useHistory();
-    
-    const Sender= useRef<HTMLIonInputElement>(null);
-    const Reciever = useRef<HTMLIonInputElement>(null);
-    const Message = useRef<HTMLIonInputElement>(null);
-
-  
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const profile = useSelector((state: AppState) => state.user.profile);
+  const params = useParams();
+  const Sender= profile?.email;
+  const Reciever = params;
+  console.log(Reciever['user']);
+  const Message = useRef<HTMLIonInputElement>(null);
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
   
-      const reciever = Reciever.current!.value;
-      const sender = Sender.current!.value;
       const body = Message.current!.value;
      
       const Mail = {
-        reciever: String(reciever),
-        sender: String(sender),
+        reciever: String(Reciever['user']),
+        sender: String(Sender),
         text: String(body)
       };
 
@@ -52,14 +51,6 @@ import { useHistory } from "react-router-dom";
           </IonHeader>
         <IonContent>
         <form className="ion-padding" onSubmit={handleSubmit}>
-        <IonItem>
-          <IonLabel position="floating">Reciever?</IonLabel>
-          <IonInput type="text" ref={Reciever}></IonInput>
-        </IonItem>
-        <IonItem>
-          <IonLabel position="floating">Your Name?</IonLabel>
-          <IonInput type="text" ref={Sender}></IonInput>
-        </IonItem>
         <IonItem>
           <IonLabel position="floating">What is your Question?</IonLabel>
           <IonInput type="text" ref={Message}></IonInput>
