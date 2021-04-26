c511bc35b33b80733e22f748406952ed174d433d
philliphnguyen <phillip.nguyen01@sjsu.edu>
2021-04-26 16:39:31 -0700

    order history done

--- client/src/pages/Orders.tsx
+++ client/src/pages/Orders.tsx
@@ -1,29 +1,86 @@
 import {
-    IonBackButton,
-    IonButtons,
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonToolbar,
-  } from "@ionic/react";
-  import React from "react";
-  
-  const Orders: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar className="ion-padding-top">
-            <IonButtons slot="start">
-              <IonBackButton defaultHref="" />
-            </IonButtons>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent>
-          <h1>Orders</h1>
-        </IonContent>
-      </IonPage>
-    );
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonItem,
+  IonList,
+  IonListHeader,
+  IonPage,
+  IonToolbar,
+} from "@ionic/react";
+import axios from "axios";
+import { chevronForwardOutline } from "ionicons/icons";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+
+const Orders: React.FC = () => {
+  const [orders, setOrders] = useState<any>([{ items: [{dish: {}}] }]);
+  const history = useHistory();
+
+  useEffect(() => {
+    axios
+      .get("/orders")
+      .then((res) => {
+        setOrders(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const dishPicStyle = {
+    width: "150px",
+    height: "150px",
+    marginRight: "10px",
   };
-  
-  export default Orders;
-  
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar className="ion-padding-top">
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="" />
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          {orders.map((order) => {
+            return (
+              <div>
+                <IonListHeader>
+                  {order.date} Total: ${order.total}
+                </IonListHeader>
+                {order.items.map((dish) => {
+                  return (
+                    <IonItem
+                      className="ion-margin-top"
+                      onClick={() => {
+                        history.push(`/user/page/${dish.dish._id}`);
+                      }}
+                    >
+                      <IonImg
+                        style={dishPicStyle}
+                        src={`../../../${dish.dish.foodPicture}`}
+                      ></IonImg>
+                      {dish.dish.name}
+                      <br />
+                      <br />${dish.dish.price}
+                      <br />
+                      <br />Quantity: {dish.quantity}
+                      <IonIcon slot="end" icon={chevronForwardOutline} />
+                    </IonItem>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Orders;
--- models/Order.js
+++ models/Order.js
@@ -4,6 +4,8 @@ const OrderSchema = new mongoose.Schema(
   {
     _id: mongoose.Schema.Types.ObjectId,
     payment_id: { type: String, required: true },
+    date: { type: String },
+    total: { type: Number },
     address: {
         street: { type : String },
         city: { type: String },
