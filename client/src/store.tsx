c9895281af5b0fee566731c89310f9938fc1dd4b
philliphnguyen <phillip.nguyen01@sjsu.edu>
2021-04-29 12:15:30 -0700

    removed redux

--- client/src/store.tsx
+++ client/src/store.tsx
@@ -5,7 +5,7 @@ import { persistStore } from 'redux-persist';
 
 export const store = createStore(allReducers, 
     compose(applyMiddleware(thunk),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    
     ));
 
 export const persistor = persistStore(store);
